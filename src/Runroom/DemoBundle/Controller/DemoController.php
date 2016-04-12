73ce6c25295384f51a38911fb1ec1750b9f5477d
Adrian Señor <adrian@runroom.com>
2016-04-12 19:25:47 +0200

    added service to controller

--- src/Runroom/DemoBundle/Controller/DemoController.php
+++ src/Runroom/DemoBundle/Controller/DemoController.php
@@ -4,19 +4,29 @@ namespace Runroom\DemoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 
+use Runroom\DemoBundle\Service\DemoService;
+
 class DemoController
 {
     protected $renderer;
+    protected $service;
 
     const INDEX_VIEW = 'demo/index.html.twig';
 
-    public function __construct(EngineInterface $renderer)
-    {
+    public function __construct(
+        EngineInterface $renderer,
+        DemoService $service
+    ) {
         $this->renderer = $renderer;
+        $this->service = $service;
     }
 
     public function index()
     {
-        return $this->renderer->renderResponse(self::INDEX_VIEW, []);
+        $model = $this->service->getDemoViewModel();
+
+        return $this->renderer->renderResponse(self::INDEX_VIEW, [
+            'model' => $model
+        ]);
     }
 }
--- src/Runroom/DemoBundle/Tests/Unit/DemoControllerTest.php
+++ src/Runroom/DemoBundle/Tests/Unit/DemoControllerTest.php
@@ -7,11 +7,17 @@ use Runroom\DemoBundle\Controller\DemoController;
 class DemoControllerTest extends \PHPUnit_Framework_TestCase
 {
     const INDEX_VIEW = 'demo/index.html.twig';
+    const MODEL = 'model';
 
     public function setUp()
     {
         $this->renderer = $this->prophesize('Symfony\Bundle\FrameworkBundle\Templating\EngineInterface');
-        $this->controller = new DemoController($this->renderer->reveal());
+        $this->service = $this->prophesize('Runroom\DemoBundle\Service\DemoService');
+
+        $this->controller = new DemoController(
+            $this->renderer->reveal(),
+            $this->service->reveal()
+        );
     }
 
     /**
@@ -20,8 +26,14 @@ class DemoControllerTest extends \PHPUnit_Framework_TestCase
     public function renderIndex()
     {
         $expected_response = new \stdClass();
-        $this->renderer->renderResponse(self::INDEX_VIEW, [])
+        $expected_model = new \stdClass();
+
+        $this->service->getDemoViewModel()
+            ->willReturn($expected_model);
+
+        $this->renderer->renderResponse(self::INDEX_VIEW, [self::MODEL => $expected_model])
             ->willReturn($expected_response);
+
         $response = $this->controller->index();
 
         $this->assertSame($expected_response, $response);
