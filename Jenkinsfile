d2b9d2c2f2b4544e4a942be5fdf3a7d544a2fb5f
Jordi Sala <jordism91@gmail.com>
2016-04-13 22:48:30 +0200

    trying to add Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,21 @@
+#!groovy
+
+node {
+
+  stage 'SCM Checkout'
+
+  checkout scm
+
+  stage 'Build'
+
+  sh 'curl -sS https://getcomposer.org/installer | php'
+  sh 'php composer.phar selfupdate'
+  sh 'php composer.phar install'
+
+  stage 'Test'
+
+  sh 'bin/phpunit -c app --coverage-clover coverage/coverage.xml --log-junit coverage/unitreport.xml'
+  step([$class: 'CloverPublisher', cloverReportDir: 'coverage', cloverReportFileName: 'coverage.xml'])
+  step([$class: 'JUnitResultArchiver', testResults: 'coverage/unitreport.xml'])
+
+}
--- app/DoctrineMigrations/Version20160412160958.php
+++ app/DoctrineMigrations/Version20160412160958.php
@@ -21,6 +21,9 @@ class Version20160412160958 extends AbstractMigration
         $this->addSql('CREATE TABLE demo_translations (id INT AUTO_INCREMENT NOT NULL, translatable_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, locale VARCHAR(255) NOT NULL, INDEX IDX_7DFB23712C2AC5D3 (translatable_id), UNIQUE INDEX demo_translations_unique_translation (translatable_id, locale), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE demo (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE demo_translations ADD CONSTRAINT FK_7DFB23712C2AC5D3 FOREIGN KEY (translatable_id) REFERENCES demo (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE demo ADD picture INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE demo ADD CONSTRAINT FK_D642DFA016DB4F89 FOREIGN KEY (picture) REFERENCES media__media (id)');
+        $this->addSql('CREATE INDEX IDX_D642DFA016DB4F89 ON demo (picture)');
     }
 
     /**
@@ -32,6 +35,7 @@ class Version20160412160958 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE demo_translations DROP FOREIGN KEY FK_7DFB23712C2AC5D3');
+        $this->addSql('ALTER TABLE demo DROP FOREIGN KEY FK_D642DFA016DB4F89');
         $this->addSql('DROP TABLE demo_translations');
         $this->addSql('DROP TABLE demo');
     }
