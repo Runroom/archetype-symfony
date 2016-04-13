d91add496ce4fea9d5eb431dd15cd809b1fae1fa
Jordi Sala <jsala@runroom.com>
2016-04-13 17:00:07 +0200

    added env vars

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,28 +4,22 @@ lock '3.4.0'
 set :application, 'gpcasinos_web'
 set :repo_url, 'git@bitbucket.org:runroom/gpcasinos_web.git'
 
-set :linked_dirs, fetch(:linked_dirs, []).push('app/logs', 'app/cache', 'vendor')
-set :linked_files, fetch(:linked_files, []).push('app/config/parameters.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('app/logs', 'app/cache', 'vendor', 'web/bundles', 'web/uploads')
+set :linked_files, fetch(:linked_files, []).push('app/config/parameters.yml', 'web/.htaccess', 'web/robots.txt')
 set :use_sudo, false
 set :keep_releases, 5
 
-namespace :deploy do
+set :default_env, {
+  'SYMFONY_ENV' => 'prod'
+}
 
-  after :updating, :create_release do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # fetch(:linked_dirs).each do |dir|
-      #   source = shared_path.join(dir)
-      #   execute 'chmod', "g+w #{source}"
-      # end
-    end
-  end
+namespace :deploy do
 
   after :updated, :migrate do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # within release_path do
-      #   execute 'php', 'bin/doctrine-dbal migrations:migrate  --no-interaction'
-      # end
-      # execute 'rm', "-f #{shared_path}/cache/class_index.php"
+      within release_path do
+        execute 'php', 'app/console migrations:migrate  --no-interaction'
+      end
     end
   end
 
