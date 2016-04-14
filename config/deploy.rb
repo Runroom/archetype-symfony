659de19e405a8a859ce3e8d61cb49e1b7896457f
Jordi Sala <jsala@runroom.com>
2016-04-14 12:49:06 +0200

    fixing composer, and deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,7 @@ lock '3.4.0'
 set :application, 'gpcasinos_web'
 set :repo_url, 'git@bitbucket.org:runroom/gpcasinos_web.git'
 
+set :composer_install_flags, '--no-dev --no-interaction --optimize-autoloader'
 set :linked_dirs, fetch(:linked_dirs, []).push('app/logs', 'app/cache', 'vendor', 'web/bundles', 'web/uploads')
 set :linked_files, fetch(:linked_files, []).push('app/config/parameters.yml', 'web/.htaccess', 'web/robots.txt')
 set :use_sudo, false
@@ -18,7 +19,7 @@ namespace :deploy do
   after :updated, :migrate do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       within release_path do
-        # execute 'php', 'app/console doctrine:migrations:migrate  --no-interaction'
+        execute 'php', 'app/console doctrine:migrations:migrate  --no-interaction'
       end
     end
   end
