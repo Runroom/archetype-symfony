9d73d8abbbc22523ad212d42b1432a8c58a9812d
Jordi Sala <jordism91@gmail.com>
2016-05-21 11:38:50 +0200

    updated to php 5.6

--- src/Runroom/DemoBundle/Admin/DemoAdmin.php
+++ src/Runroom/DemoBundle/Admin/DemoAdmin.php
@@ -2,13 +2,13 @@
 
 namespace Runroom\DemoBundle\Admin;
 
-use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Show\ShowMapper;
 
-class DemoAdmin extends Admin
+class DemoAdmin extends AbstractAdmin
 {
     /**
      * @param DatagridMapper $datagridMapper
