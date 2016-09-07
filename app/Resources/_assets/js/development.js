e555618e08dfa17ad1c7d70e48600d669741580f
Italo <italodr@gmail.com>
2016-09-07 10:01:47 +0200

    Delete unused directories, Update gitignore, Update package.json

--- app/Resources/_assets/js/development.js
+++
@@ -1,9 +0,0 @@
-$('input[type="checkbox"]').on('change', function(event) {
-    event.preventDefault();
-    var value = $(this).val();
-    if (this.checked) {
-        $('html').addClass(value);
-    } else {
-        $('html').removeClass(value);
-    }
-});
