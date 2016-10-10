5e6fff0e5ef5e28c27575b82bde2f8d2edd774a0
Joel <joel@runroom.com>
2016-10-10 17:19:17 +0200

    Create Admin entity for Messages

---
+++ src/Runroom/TranslationsBundle/Admin/MessageAdmin.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Runroom\TranslationsBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class MessageAdmin extends AbstractAdmin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('key')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('key')
+            ->add('value', null, [
+                'sort_field_mapping' => ['fieldName' => 'value'],
+                'sort_parent_association_mappings' => [['fieldName' => 'translations']]
+            ])
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('key')
+            ->add('translations', 'a2lix_translations', [
+                'cascade_validation' => true,
+                'fields' => [
+                    'value' => ['required' => false, 'field_type' => 'ckeditor'],
+                ]
+            ])
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('key')
+            ->add('value')
+        ;
+    }
+}
