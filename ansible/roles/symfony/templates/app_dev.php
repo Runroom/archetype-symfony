8da2b1f9511ba3afffbb9b819de75f3272213601
Jordi Sala <jordism91@gmail.com>
2016-11-26 13:28:55 +0100

    removed useles comments

--- ansible/roles/symfony/templates/app_dev.php
+++ ansible/roles/symfony/templates/app_dev.php
@@ -3,11 +3,6 @@
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read http://symfony.com/doc/current/book/installation.html#checking-symfony-application-configuration-and-setup
-// for more information
-// umask(0002);
-
 /*
  * @var Composer\Autoload\ClassLoader
  */
