ecabda88e96a5456c7ff3334d1fcddb39bfd6f33
Jordi Sala <jsala@runroom.com>
2016-12-22 16:45:59 +0100

    fixing autoload

--- app/autoload.php
+++ app/autoload.php
@@ -3,7 +3,10 @@
 use Composer\Autoload\ClassLoader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
-require __DIR__ . '/../vendor/autoload.php';
+/*
+ * @var ClassLoader
+ */
+$loader = require __DIR__ . '/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
