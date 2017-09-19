b6c418399e7cd226212f6ccb264244178875a40c
Jordi Sala Morales <jordism91@gmail.com>
2017-09-19 15:32:36 +0000

    Merged in hotfix/app (pull request #55)
    Disable HTTP Cache
    
    Approved-by: Adrián Señor <adrian@runroom.com>
    Approved-by: Jordi Sala Morales <jordism91@gmail.com>

--- web/app.php
+++ web/app.php
@@ -8,9 +8,11 @@ use Symfony\Component\HttpFoundation\Request;
 require __DIR__ . '/../app/autoload.php';
 
 $kernel = new AppKernel('prod', false);
-$kernel = new AppCache($kernel);
 
-Request::enableHttpMethodParameterOverride();
+// If you want to use HTTP Cache, just uncomment the following PHP lines
+// $kernel = new AppCache($kernel);
+// Request::enableHttpMethodParameterOverride();
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
