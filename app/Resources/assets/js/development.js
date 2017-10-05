18a7638cdcc5a8169bea5fbdb90136e79a32fbae
Adrián Señor <adrian@runroom.com>
2017-10-05 13:55:51 +0000

    Merged in feature/improve-developmentjs (pull request #60)
    Improvements over development js
    
    Approved-by: ivan-runroom <ivan@runroom.com>
    Approved-by: Jordi Sala Morales <jordism91@gmail.com>

--- app/Resources/assets/js/development.js
+++ app/Resources/assets/js/development.js
@@ -1,28 +1,21 @@
 'use strict';
 
-document.querySelectorAll('.js-gridAction').forEach(action => {
-    action.addEventListener('change', function(event) {
-        event.preventDefault();
-        const value = this.value;
-        const html = document.querySelector('html');
-        if (this.checked) {
-            html.classList.add(value);
-        } else {
-            html.classList.remove(value);
-        }
+document.querySelectorAll('.js-gridAction')
+    .forEach(action => {
+        action.addEventListener('change', function(event) {
+            event.preventDefault();
+            document.querySelector('html').classList.toggle(this.value);
+        });
     });
-});
 
-document.querySelectorAll('.js-closeGridToggle').forEach(action => {
-    action.addEventListener('click', event => {
+document.querySelector('.js-closeGridToggle')
+    .addEventListener('click', event => {
         event.preventDefault();
         document.querySelector('.dev-Grid-toggle').classList.remove('is-open');
     });
-});
 
-document.querySelectorAll('.js-openGridToggle').forEach(action => {
-    action.addEventListener('click', event => {
+document.querySelector('.js-openGridToggle')
+    .addEventListener('click', event => {
         event.preventDefault();
         document.querySelector('.dev-Grid-toggle').classList.add('is-open');
     });
-});
