858f12560c4c39fb95ca565653e5e61dfe3f83b6
Jordi Sala Morales <jordism91@gmail.com>
2018-01-08 14:27:43 +0000

    Merged in hotfix/deployer (pull request #103)
    Downgrade de Deployer
    
    * Downgrade deployer version to fix deploys failing on Jenkins
    
    * Update dependencies
    
    Approved-by: Adrián Señor <adrian@runroom.com>
    Approved-by: Jordi Sala Morales <jordism91@gmail.com>

--- deploy.php
+++ deploy.php
@@ -10,14 +10,18 @@ set('shared_files', ['app/config/parameters.yml', 'web/robots.txt']); // 'web/.h
 set('writable_dirs', ['var/logs', 'var/cache', 'var/spool', 'web/uploads']);
 set('clear_paths', ['web/app_dev.php']);
 
+set('ssh_type', 'native');
+set('ssh_multiplexing', true);
+set('env', 'prod');
+set('env_vars', 'SYMFONY_ENV={{env}}');
+
 set('allow_anonymous_stats', false);
-set('env', ['SYMFONY_ENV' => 'prod']);
 set('console', '{{release_path}}/bin/console');
 set('composer_options', '{{composer_action}} --no-dev --prefer-dist --no-progress --no-interaction --classmap-authoritative');
 
 task('app', function () {
-    run('{{bin/php}} {{console}} cache:warmup --no-interaction');
-    run('{{bin/php}} {{console}} doctrine:migrations:migrate --no-interaction');
+    run('{{env_vars}} {{bin/php}} {{console}} cache:warmup --no-interaction');
+    run('{{env_vars}} {{bin/php}} {{console}} doctrine:migrations:migrate --no-interaction');
 })->setPrivate();
 
 after('deploy:update_code', 'deploy:clear_paths');
@@ -25,4 +29,4 @@ after('deploy:vendors', 'deploy:writable');
 after('deploy:writable', 'app');
 after('deploy:failed', 'deploy:unlock');
 
-inventory('servers.yml');
+serverList('servers.yml');
