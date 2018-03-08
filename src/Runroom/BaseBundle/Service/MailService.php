9d66b121faf13b94625a7661be71e663f3fa5df1
Jordi Sala Morales <jordism91@gmail.com>
2018-03-08 10:27:17 +0000

    Merged in feature/mail-service (pull request #117)
    Add mail service
    
    * Add mail service
    
    * Improve mail configuration
    
        Send mails from dev environment, without configuring anything
    
    * Add test for mailService
    
    Approved-by: Adrián Señor <adrian@runroom.com>
    Approved-by: Jordi Sala Morales <jordism91@gmail.com>
    Approved-by: Jorge Agundez Medina <jagundez@runroom.com>

---
+++ src/Runroom/BaseBundle/Service/MailService.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Runroom\BaseBundle\Service;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Translation\TranslatorInterface;
+use Twig\Environment;
+
+class MailService
+{
+    protected $mailer;
+    protected $twig;
+    protected $translator;
+    protected $requestStack;
+    protected $from;
+    protected $bcc;
+    protected $locale;
+
+    public function __construct(
+        \Swift_Mailer $mailer,
+        Environment $twig,
+        TranslatorInterface $translator,
+        RequestStack $requestStack,
+        string $from,
+        array $bcc
+    ) {
+        $this->mailer = $mailer;
+        $this->twig = $twig;
+        $this->translator = $translator;
+        $this->requestStack = $requestStack;
+        $this->from = $from;
+        $this->bcc = $bcc;
+    }
+
+    public function send(string $to, string $subject, string $template, array $parameters = []): void
+    {
+        $parameters = \array_merge($parameters, [
+            'locale' => $this->getLocale(),
+        ]);
+
+        $message = $this->mailer->createMessage()
+            ->setFrom([$this->from => $this->translate('mail.from_name')])
+            ->setTo($to)
+            ->setBCC($this->bcc)
+            ->setSubject($this->translate($subject))
+            ->setBody(
+                $this->twig->render($template . '.html.twig', $parameters),
+                'text/html'
+            )
+            ->addPart(
+                $this->twig->render($template . '.txt.twig', $parameters),
+                'text/plain'
+            );
+
+        $this->mailer->send($message);
+    }
+
+    public function setLocale(string $locale): void
+    {
+        $this->locale = $locale;
+    }
+
+    public function getLocale(): string
+    {
+        if (\is_null($this->locale)) {
+            $this->locale = $this->requestStack->getCurrentRequest()->getLocale();
+        }
+
+        return $this->locale;
+    }
+
+    protected function translate(string $key)
+    {
+        return $this->translator->trans($key, [], null, $this->getLocale());
+    }
+}
---
+++ tests/Runroom/BaseBundle/Unit/MailServiceTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Runroom\BaseBundle\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Runroom\BaseBundle\Service\MailService;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Translation\TranslatorInterface;
+use Twig\Environment;
+
+class MailServiceTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->mailer = $this->prophesize(\Swift_Mailer::class);
+        $this->twig = $this->prophesize(Environment::class);
+        $this->translator = $this->prophesize(TranslatorInterface::class);
+        $this->requestStack = $this->prophesize(RequestStack::class);
+
+        $this->service = new MailService(
+            $this->mailer->reveal(),
+            $this->twig->reveal(),
+            $this->translator->reveal(),
+            $this->requestStack->reveal(),
+            'from@symfony.local',
+            ['bcc@symfony.local']
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function itSendsAnEmail()
+    {
+        $this->requestStack->getCurrentRequest()->willReturn(new Request());
+        $this->translator->trans('mail.from_name', [], null, 'en')->shouldBeCalled();
+        $this->translator->trans('subject', [], null, 'en')->shouldBeCalled();
+        $this->twig->render('template.html.twig', ['locale' => 'en'])->shouldBeCalled();
+        $this->twig->render('template.txt.twig', ['locale' => 'en'])->shouldBeCalled();
+        $this->mailer->createMessage()->willReturn(new \Swift_Message());
+        $this->mailer->send(Argument::type(\Swift_Message::class))->shouldBeCalled();
+
+        $this->service->send('to@symfony.local', 'subject', 'template');
+    }
+
+    /**
+     * @test
+     */
+    public function itSendsAnEmailWithSpecificLocale()
+    {
+        $this->translator->trans('mail.from_name', [], null, 'es')->shouldBeCalled();
+        $this->translator->trans('subject', [], null, 'es')->shouldBeCalled();
+        $this->twig->render('template.html.twig', ['locale' => 'es'])->shouldBeCalled();
+        $this->twig->render('template.txt.twig', ['locale' => 'es'])->shouldBeCalled();
+        $this->mailer->createMessage()->willReturn(new \Swift_Message());
+        $this->mailer->send(Argument::type(\Swift_Message::class))->shouldBeCalled();
+
+        $this->service->setLocale('es');
+        $this->service->send('to@symfony.local', 'subject', 'template');
+    }
+}
