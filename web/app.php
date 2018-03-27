5fb8eab9ee66680846a2988712812f1fddded64e
Jordi Sala Morales <jordism91@gmail.com>
2018-03-27 10:15:36 +0000

    Merged in hotfix/configuration (pull request #120)
    Move .htaccess to web directly, remove useless comment
    
    Approved-by: Adrián Señor <adrian@runroom.com>
    Approved-by: Jordi Sala Morales <jordism91@gmail.com>

--- web/app.php
+++ web/app.php
@@ -2,9 +2,6 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// umask(0002);
-
 require __DIR__ . '/../app/autoload.php';
 
 $kernel = new AppKernel('prod', false);
