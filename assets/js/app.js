51bd55a7d79039eb3bc567696a0fdc30d9d61c66
Italo Devoto Ramella <italodr@gmail.com>
2019-11-11 10:38:20 +0000

    Merged in feature/sass-refactor (pull request #245)
    Modular Sass
    
    * Remove obsolete and unused classes
    
    * Remove unnecesary styles, Update stylelint version, Add indent of 2 for json files
    
    * Add pointer events none to svg inside anchors
    
    * Reset neutro colors
    
    * Remove variables from button atom
    
    * Create new mixins for class generation, Remove unneeded files
    
    * Add comment
    
    * Create flex mixins
    
    * Move invocations to demo file
    
    * Add flex-grid as mixin
    
    * Update wysiwyg
    
    * Add TODOs
    
    * Unificate names
    
    * Add Scroll as mixin
    
    * Add Scroll as mixin
    
    * Add show and hide visibility tool into demo page
    
    * Add u-sr-only example into demo page
    
    * Sass modular mixins
    
    * Exclude composer.json from editorconfig json files
    
    * Add visibility to demo, Fix issues with loop of breakpoints
    
    * Fixing demo
    
    * Fix link to ajax form
    
    * Design refactor, Add new layout for testing, Add new components
    
    * Remove dots from demo checkboxes
    
    * Add component reference
    
    * Fix issue with cookies message style
    
    * Refactor cookies page
    
    * Refactor statics
    
    Approved-by: Jose Sentís <jose@runroom.com>
    Approved-by: Jordi Sala Morales <jordism91@gmail.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-import { scrollTo, events, touchable } from '@runroom/purejs';
+import { forEach, scrollTo, events, touchable } from '@runroom/purejs';
 
 // In order to keep readability and maintainability on bigger projects
 // we recommend to use module import method and import it as needed.
@@ -12,16 +12,19 @@ if (!!window.MSInputMethodContext && !!document.documentMode) {
 }
 
 events.onDocumentReady(() => {
-  const anchor = document.querySelector('.js-anchor');
-
   cookies();
 
   // For small projects or low use of javascript, you can add events in this
   // same file, as follows. Eventhough the module import method is preferred.
-  if (anchor) {
-    anchor.addEventListener('click', event => {
-      const target = event.target.getAttribute('data-anchor');
-      scrollTo.animate(target, 300);
+  const anchors = document.querySelectorAll('.js-anchor');
+
+  if (anchors) {
+    forEach(anchors, anchor => {
+      anchor.addEventListener('click', event => {
+        const target =
+          event.target.getAttribute('data-anchor') || event.target.getAttribute('href');
+        scrollTo.animate(target, 300);
+      });
     });
   }
 });
--- gulpfile.babel.js/tasks/styles.colors.js
+++ gulpfile.babel.js/tasks/styles.colors.js
@@ -5,7 +5,7 @@ import plumber from 'gulp-plumber';
 import { STYLES_SRC, VIEWS_DEST } from '../config/routes';
 import errorAlert from '../config/fn.error.alert';
 
-const STYLE_COLORS = [`${STYLES_SRC}/settings/_colors.scss`];
+const STYLE_COLORS = [`${STYLES_SRC}/settings/variables/_colors.scss`];
 const STYLE_COLORS_DEST = [`${VIEWS_DEST}/styleguide/data`];
 
 const sassjson = () => {
