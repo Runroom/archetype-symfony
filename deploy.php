e384ea21d0c76fbe6a45a0b8cdfdbd6943bce891
Jordi <jordism91@gmail.com>
2020-02-12 10:50:52 +0100

    Update deployer

--- deploy.php
+++ deploy.php
@@ -9,9 +9,6 @@ set('shared_dirs', ['var/spool', 'public/uploads']);
 set('shared_files', ['.env.local', 'public/robots.txt']);
 set('writable_dirs', ['var/log', 'var/cache', 'var/spool', 'public/uploads']);
 
-set('ssh_type', 'native');
-set('ssh_multiplexing', true);
-
 set('allow_anonymous_stats', false);
 set('console', '{{release_path}}/bin/console');
 set('composer_options', '{{composer_action}} --prefer-dist --classmap-authoritative --no-progress --no-interaction --no-dev');
@@ -23,6 +20,10 @@ set('bin/yarn', function () {
 task('app', function () {
     cd('{{release_path}}');
 
+    if (has('previous_release')) {
+        run('cp -R {{previous_release}}/vendor {{release_path}}/vendor');
+    }
+
     run('{{bin/php}} {{bin/composer}} symfony:dump-env prod');
     run('{{bin/php}} {{console}} cache:warmup --no-interaction');
     run('{{bin/php}} {{console}} doctrine:migrations:migrate --no-interaction');
@@ -31,12 +32,16 @@ task('app', function () {
 task('yarn:build', function () {
     cd('{{release_path}}');
 
+    if (has('previous_release')) {
+        run('cp -R {{previous_release}}/node_modules {{release_path}}/node_modules');
+    }
+
     run('. ~/.nvm/nvm.sh --no-use && nvm use && {{bin/yarn}} && {{bin/yarn}} encore production');
 })->setPrivate();
 
-after('deploy:vendors', 'deploy:writable');
-after('deploy:writable', 'yarn:build');
+after('deploy:vendors', 'yarn:build');
 after('yarn:build', 'app');
 after('deploy:failed', 'deploy:unlock');
 
-serverList('servers.yaml');
+inventory('servers.yaml')
+    ->user(\getenv('DEPLOYER_USER'));
