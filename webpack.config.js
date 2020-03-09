610fdcfa7b75a0a3e8c2d37676ffed867e61751c
Jordi <jordism91@gmail.com>
2020-03-09 15:04:54 +0100

    Remove inline validation js and ckeditor

--- webpack.config.js
+++ webpack.config.js
@@ -6,25 +6,17 @@ const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 Encore.setOutputPath('public/build/')
   .setPublicPath('/build')
-  .copyFiles({
-    from: './assets/img',
-    to: 'img/[name].[contenthash].[ext]',
-    pattern: /\.(png|jpg|jpeg|gif|ico)$/
-  })
-  .copyFiles({
-    from: './assets/img',
-    to: 'svg/[name].svg',
-    pattern: /\.svg$/
-  })
-  .copyFiles({
-    from: './assets/fonts',
-    to: 'fonts/[name].[contenthash].[ext]',
-    pattern: /\.(woff|woff2)$/
-  })
-  .configureFilenames({
-    js: 'js/[name].[contenthash].js',
-    css: 'css/[name].[contenthash].css'
-  })
+  .copyFiles([
+    {from: './assets/img', to: 'img/[name].[contenthash].[ext]', pattern: /\.(png|jpg|jpeg|gif|ico)$/},
+    {from: './assets/img', to: 'svg/[name].svg', pattern: /\.svg$/},
+    {from: './assets/fonts', to: 'fonts/[name].[contenthash].[ext]', pattern: /\.(woff|woff2)$/},
+
+    {from: './node_modules/ckeditor4/', to: 'ckeditor/[path][name].[ext]', pattern: /\.(js|css)$/, includeSubdirectories: false},
+    {from: './node_modules/ckeditor4/adapters', to: 'ckeditor/adapters/[path][name].[ext]'},
+    {from: './node_modules/ckeditor4/lang', to: 'ckeditor/lang/[path][name].[ext]'},
+    {from: './node_modules/ckeditor4/plugins', to: 'ckeditor/plugins/[path][name].[ext]'},
+    {from: './node_modules/ckeditor4/skins', to: 'ckeditor/skins/[path][name].[ext]'}
+  ])
   .enableSingleRuntimeChunk()
   .cleanupOutputBeforeBuild()
   .enableBuildNotifications()
@@ -46,16 +38,15 @@ Encore.setOutputPath('public/build/')
       paths: glob.sync([path.join(__dirname, 'templates/**/*.html.twig')]),
       whitelist: ['non-touch'],
       whitelistPatterns: [/^js-/, /^u-/],
-      extractors: [
-        {
-          extractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-          extensions: ['twig']
-        }
-      ]
+      extractors: [{
+        extractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+        extensions: ['twig']
+      }]
     })
   )
   .enablePostCssLoader()
   .addEntry('app', './assets/js/app.js')
+  .addEntry('form', './assets/js/form.js')
   .addEntry('development-scripts', './assets/js/development.js')
   .addEntry('styleguide-scripts', './assets/js/styleguide.js')
   .addStyleEntry('styles', './assets/scss/styles.scss')
