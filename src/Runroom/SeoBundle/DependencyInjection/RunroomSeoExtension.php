49b0b47d312bf36a80bf6e3c6ba9df15542f70cf
Jordi <jordism91@gmail.com>
2020-04-28 18:00:52 +0200

    Fix Base name

--- src/Runroom/SeoBundle/DependencyInjection/RunroomSeoExtension.php
+++ src/Runroom/SeoBundle/DependencyInjection/RunroomSeoExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class RunroomBaseExtension extends Extension
+class RunroomSeoExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
