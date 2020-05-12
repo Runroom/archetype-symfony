33117f3896d4082ae9a5d6fab61749ea28cea0ff
Jordi <jordism91@gmail.com>
2020-05-12 13:25:34 +0200

    Avoid session start to get flash messages, mark responses as private when using cookies

--- src/App/EventSubscriber/ClientIpSubscriber.php
+++ src/App/EventSubscriber/ClientIpSubscriber.php
@@ -21,6 +21,7 @@ class ClientIpSubscriber implements EventSubscriberInterface
             $response->headers->setCookie(
                 Cookie::create(self::COOKIE_NAME, $request->getClientIp(), 0, '/', null, true, false)
             );
+            $response->setPrivate();
         }
     }
 
