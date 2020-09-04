fed624803d0b6a03f800a96801a03b5035add3d4
Jordi <jordism91@gmail.com>
2020-09-04 10:30:08 +0200

    fix composer-v2 path

--- deploy.php
+++ deploy.php
@@ -17,7 +17,9 @@ set('allow_anonymous_stats', false);
 set('console', 'bin/console');
 set('composer_options', '{{composer_action}} --prefer-dist --classmap-authoritative --no-progress --no-interaction --no-dev');
 
-set('bin/composer', 'composer-v2');
+// Remove this when only using Composer at version 2
+set('bin/composer', '/usr/local/bin/composer-v2');
+
 set('bin/yarn', function () {
     return run('which yarn');
 });
