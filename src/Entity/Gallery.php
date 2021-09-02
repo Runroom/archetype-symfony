c4f0ec806448cbf7d687780f37457f9de86085e1
Jordi <jordism91@gmail.com>
2021-09-02 10:10:25 +0200

    Remove Gallery class and GalleryItem class

--- src/Entity/Gallery.php
+++ src/Entity/Gallery.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
+use Sonata\MediaBundle\Entity\BaseGallery;
 
-/** @ORM\Entity */
-class Gallery
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="media__gallery")
+ */
+class Gallery extends BaseGallery
 {
     /**
      * @ORM\Id
@@ -19,51 +20,8 @@ class Gallery
      */
     private ?int $id = null;
 
-    /**
-     * @var Collection<int, GalleryImage>
-     *
-     * @Assert\Valid
-     * @ORM\OneToMany(targetEntity="GalleryImage", mappedBy="gallery", cascade={"all"})
-     * @ORM\OrderBy({"position" = "ASC"})
-     */
-    private Collection $galleryImages;
-
-    public function __construct()
-    {
-        $this->galleryImages = new ArrayCollection();
-    }
-
-    public function setId(?int $id): self
-    {
-        $this->id = $id;
-
-        return $this;
-    }
-
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function addGalleryImage(GalleryImage $galleryImage): self
-    {
-        $this->galleryImages[] = $galleryImage;
-
-        $galleryImage->setGallery($this);
-
-        return $this;
-    }
-
-    public function removeGalleryImage(GalleryImage $galleryImage): void
-    {
-        $this->galleryImages->removeElement($galleryImage);
-
-        $galleryImage->setGallery(null);
-    }
-
-    /** @return Collection<int, GalleryImage> */
-    public function getGalleryImages(): Collection
-    {
-        return $this->galleryImages;
     }
 }
