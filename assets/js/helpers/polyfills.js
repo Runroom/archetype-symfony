eb54abc84f43ef4ef6c40ff8d0d7bf23da2a60c5
Jordi Sala Morales <jordism91@gmail.com>
2022-01-10 16:50:41 +0100

    Check prettier on github actions

--- assets/js/helpers/polyfills.js
+++ assets/js/helpers/polyfills.js
@@ -1,5 +1,5 @@
 if (!('remove' in Element.prototype)) {
-  Element.prototype.remove = function() {
+  Element.prototype.remove = function () {
     if (this.parentNode) {
       this.parentNode.removeChild(this);
     }
--- assets/js/helpers/utils.js
+++ assets/js/helpers/utils.js
@@ -26,11 +26,12 @@ const keyup = (key, callback) => {
   });
 };
 
-const preloadImage = url => new Promise((resolve, reject) => {
-  const image = new Image();
-  image.src = url;
-  image.onload = resolve;
-  image.onerror = reject;
-});
+const preloadImage = url =>
+  new Promise((resolve, reject) => {
+    const image = new Image();
+    image.src = url;
+    image.onload = resolve;
+    image.onerror = reject;
+  });
 
 export { keyup, preloadImage };
