4c4aafaa8ec518a8313816bed6554154eecf4382
Italo Devoto Ramella <italodr@gmail.com>
2022-01-25 15:11:39 +0100

    Improve components, Add test if not exist

--- tests/Frontend/components/skip-link.test.js
+++ tests/Frontend/components/skip-link.test.js
@@ -1,17 +1,27 @@
 /* eslint-disable */
 import { render } from '../config/testing-utils';
 
-describe('Back link functionallity', () => {
-  it('Should have correct data', async () => {
+describe('Skip links', () => {
+  it('Should render link with provided data', async () => {
+    const title = 'This is a skip link';
+    const target = '#anchor-target';
+    const attributes = 'data-testid="skip-link"';
+
     const { getByTestId } = await render(
-      './templates/components/skip-link.html.twig',
-      {
-        target: 'google.com',
-        title: 'Google'
-      }
+      './templates/components/skip-link.html.twig', { component: { attributes, target, title } }
     );
 
-    expect(getByTestId('skip-link')).toHaveAttribute('href', 'google.com');
-    expect(getByTestId('skip-link')).toHaveTextContent('Google');
+    expect(getByTestId('skip-link')).toHaveAttribute('href', target);
+    expect(getByTestId('skip-link')).toHaveTextContent(title);
+  });
+
+  it('Should not render skip link', async () => {
+    const title = 'This is a skip link';
+
+    const { queryByText } = await render(
+      './templates/components/skip-link.html.twig', { component: { title } }
+    );
+
+    expect(queryByText(title)).toBeNull();
   });
 });
