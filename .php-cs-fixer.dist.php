a023b188b3c6687ae6f08a33715876b36b932277
Jordi Sala Morales <jordism91@gmail.com>
2022-03-03 17:18:50 +0100

    Apply lint

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -30,12 +30,14 @@ $config->setRules([
     'ordered_imports' => true,
     'phpdoc_align' => ['align' => 'left'],
     'phpdoc_order' => true,
+    'phpdoc_to_comment' => ['ignored_tags' => ['psalm-suppress', 'phpstan-ignore-next-line', 'todo']],
     'compact_nullable_typehint' => true,
     'void_return' => false,
     'strict_comparison' => true,
     'strict_param' => true,
     'php_unit_strict' => true,
     'php_unit_test_annotation' => ['style' => 'annotation'],
+    'php_unit_test_case_static_method_calls' => true,
 ])
 ->setRiskyAllowed(true)
 ->setFinder($finder);
