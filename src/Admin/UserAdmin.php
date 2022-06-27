39abdf40c0b94a302a13c2647237ff3374810aa6
Jordi Sala Morales <jordism91@gmail.com>
2022-06-27 09:44:46 +0200

    Add custom admin

---
+++ src/Admin/UserAdmin.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\Form\Type\DatePickerType;
+
+/**
+ * @phpstan-extends AbstractAdmin<\Sonata\UserBundle\Model\UserInterface>
+ */
+class UserAdmin extends AbstractAdmin
+{
+    protected $baseRoutePattern = 'asda';
+    protected $baseRouteName = 'asda';
+
+    protected function configureListFields(ListMapper $list): void
+    {
+        $list->addIdentifier('id');
+    }
+
+    protected function configureFormFields(FormMapper $form): void
+    {
+        $form
+            ->add('normal_datepicker', DatePickerType::class, [
+                'mapped' => false,
+                'datepicker_options' => [
+                    'allowInput' => false,
+                    'disable' => [new \DateTime('tomorrow')],
+                ],
+            ]);
+    }
+}
