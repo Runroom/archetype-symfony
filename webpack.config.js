d073acc261819feb463a1de8c2980bc30cc6da57
Jordi Sala Morales <jordism91@gmail.com>
2022-09-21 12:11:36 +0200

    Improve ckeditor copy

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const Encore = require('@symfony/webpack-encore');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 Encore.setOutputPath('public/build/')
   .setPublicPath('/build')
@@ -11,18 +10,7 @@ Encore.setOutputPath('public/build/')
       pattern: /\.(png|jpg|jpeg|gif|ico)$/
     },
     { from: './assets/img', to: 'svg/[name].svg', pattern: /\.svg$/ },
-    { from: './assets/fonts', to: 'fonts/[name].[contenthash].[ext]', pattern: /\.(woff|woff2)$/ },
-
-    {
-      from: './node_modules/ckeditor4/',
-      to: 'ckeditor/[path][name].[ext]',
-      pattern: /\.(js|css)$/,
-      includeSubdirectories: false
-    },
-    { from: './node_modules/ckeditor4/adapters', to: 'ckeditor/adapters/[path][name].[ext]' },
-    { from: './node_modules/ckeditor4/lang', to: 'ckeditor/lang/[path][name].[ext]' },
-    { from: './node_modules/ckeditor4/plugins', to: 'ckeditor/plugins/[path][name].[ext]' },
-    { from: './node_modules/ckeditor4/skins', to: 'ckeditor/skins/[path][name].[ext]' }
+    { from: './assets/fonts', to: 'fonts/[name].[contenthash].[ext]', pattern: /\.(woff|woff2)$/ }
   ])
   .enableSingleRuntimeChunk()
   .cleanupOutputBeforeBuild(['**/*', '!.gitignore'])
@@ -35,6 +23,7 @@ Encore.setOutputPath('public/build/')
     };
   })
   .enablePostCssLoader()
+  .enableForkedTypeScriptTypesChecking()
   .enableBuildCache({ config: [__filename] })
   .enableEslintPlugin()
   .addPlugin(
@@ -43,7 +32,6 @@ Encore.setOutputPath('public/build/')
       emitWarning: true
     })
   )
-  .addPlugin(new ForkTsCheckerWebpackPlugin())
   .addEntry('app', './assets/js/app.ts')
   .addEntry('form', './assets/js/form.ts')
   .addStyleEntry('styles', './assets/css/styles.css')
