7495596540ee1697b763dcfd84a73231b8f58b1d
Aitor Errasti <azarparalelo@gmail.com>
2023-01-17 15:40:41 +0100

    Update ContentStory as global_state

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,14 +4,8 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use App\Story\ContentStory;
 use Symfony\Component\Dotenv\Dotenv;
-use Zenstruck\Foundry\Test\TestState;
 
 require \dirname(__DIR__) . '/vendor/autoload.php';
 
 (new Dotenv())->bootEnv(\dirname(__DIR__) . '/.env');
-
-TestState::addGlobalState(function (): void {
-    ContentStory::load();
-});
