4203ae5098bb145ebe7579445acacf829755a61c
Jordi Sala Morales <jordism91@gmail.com>
2023-03-09 13:06:28 +0000

    Fix setup

--- .docker/make/01_setup.mk
+++ .docker/make/01_setup.mk
@@ -1,9 +1,12 @@
 NODE_MODULES_DIR = node_modules
 CERTS_DIR = .docker/traefik/certs
+BUILD_DIR = public/build
 
 setup: ## Initial setup to get the project running.
 	mkdir --parents $(NODE_MODULES_DIR)
 	mkdir --parents $(CERTS_DIR)
+	mkdir --parents $(BUILD_DIR)
 	mkcert -install
 	mkcert -cert-file $(CERTS_DIR)/cert.crt -key-file $(CERTS_DIR)/cert.key localhost
+	echo '{}' > public/build/manifest.json
 .PHONY: setup
