e250f379664b2af861e07b9b6c17688e9226e6db
Jordi Sala Morales <jordism91@gmail.com>
2023-04-14 10:31:47 +0200

    Improve setup makefile

--- .docker/make/01_setup.mk
+++ .docker/make/01_setup.mk
@@ -4,11 +4,11 @@ BUILD_DIR = public/build
 
 setup: ## Create the directories and files needed for local development.
 	mkdir --parents $(NODE_MODULES_DIR)
-	mkdir --parents $(CERTS_DIR)
 	[ -f $(BUILD_DIR)/manifest.json ] || echo '{}' > $(BUILD_DIR)/manifest.json
 .PHONY: setup
 
 certs: ## Create a self-signed certificate for local development.
 	mkcert -install
+	mkdir --parents $(CERTS_DIR)
 	mkcert -cert-file $(CERTS_DIR)/cert.crt -key-file $(CERTS_DIR)/cert.key localhost
 .PHONY: certs
