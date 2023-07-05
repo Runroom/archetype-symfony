db8e0d4dd086527e678f5f1381640e67ff997a20
Imar Abreu <imar@runroom.com>
2023-07-05 14:57:29 +0100

    Remove tailwind dependencies

--- etc/tailwind/base/tailwind.config.js
+++
@@ -1,6 +0,0 @@
-const tailwindConfig = require('../base.config');
-
-module.exports = {
-  ...tailwindConfig,
-  content: ['templates/**/*', 'vendor/runroom/samples-bundle/src/Resources/views/**/*']
-};
