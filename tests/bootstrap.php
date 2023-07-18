789f328f20e7efc036d1e403da05d80a2518655a
Jordi Sala Morales <jordism91@gmail.com>
2023-07-18 08:54:28 +0000

    Clear cache between runs

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use App\Kernel;
 use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\Filesystem\Filesystem;
 
 require \dirname(__DIR__) . '/vendor/autoload.php';
 
 (new Dotenv())->bootEnv(\dirname(__DIR__) . '/.env');
+
+$kernel = new Kernel($_SERVER['APP_ENV'] ?? 'test', (bool) ($_SERVER['APP_DEBUG'] ?? false));
+
+(new Filesystem())->remove([$kernel->getCacheDir()]);
