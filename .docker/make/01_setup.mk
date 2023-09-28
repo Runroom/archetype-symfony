e5d2aa674b9932d658a22e587dbad902e6dd6afb
Jordi Sala Morales <jordism91@gmail.com>
2023-09-28 12:07:25 +0200

    Fix MacOS builds

--- .docker/make/01_setup.mk
+++ .docker/make/01_setup.mk
@@ -3,12 +3,14 @@ CERTS_DIR = .docker/traefik/certs
 BUILD_DIR = public/build
 
 setup: ## Create the directories and files needed for local development.
-	mkdir --parents $(NODE_MODULES_DIR)
+	# MacOS does not support --parents for the mkdir command
+	mkdir -p $(NODE_MODULES_DIR)
 	[ -f $(BUILD_DIR)/manifest.json ] || echo '{}' > $(BUILD_DIR)/manifest.json
 .PHONY: setup
 
 certs: ## Create a self-signed certificate for local development.
 	mkcert -install
-	mkdir --parents $(CERTS_DIR)
+	# MacOS does not support --parents for the mkdir command
+	mkdir -p $(CERTS_DIR)
 	mkcert -cert-file $(CERTS_DIR)/cert.crt -key-file $(CERTS_DIR)/cert.key localhost
 .PHONY: certs
