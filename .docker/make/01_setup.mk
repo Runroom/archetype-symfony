f67c2695ab30e5182e9ebebe3d5b8351c7bf6af8
Jordi Sala Morales <jordism91@gmail.com>
2023-10-24 15:40:58 +0200

    Improve docker configuration to not end up in an unusable state (#3652)
    * Improve docker configuration to not end up in an unusable state
    
    * Update .docker/make/02_docker.mk

--- .docker/make/01_setup.mk
+++ .docker/make/01_setup.mk
@@ -1,16 +1,29 @@
-NODE_MODULES_DIR = node_modules
 CERTS_DIR = .docker/traefik/certs
-BUILD_DIR = public/build
+NODE_MODULES_DIR = node_modules
+MANIFEST_FILE = public/build/manifest.json
+MKCERT := $(shell command -v mkcert 2> /dev/null)
 
-setup: ## Create the directories and files needed for local development.
-	# MacOS does not support --parents for the mkdir command
-	mkdir -p $(NODE_MODULES_DIR)
-	[ -f $(BUILD_DIR)/manifest.json ] || echo '{}' > $(BUILD_DIR)/manifest.json
+# MacOS does not support --parents for the mkdir command
+
+setup: ${CERTS_DIR} ${NODE_MODULES_DIR} ${MANIFEST_FILE} ## Create the directories and files needed for local development.
 .PHONY: setup
 
 certs: ## Create a self-signed certificate for local development.
+	mkdir -p ${CERTS_DIR}
+
+ifndef MKCERT
+	$(info No mkcert was found in PATH, install it from here: https://github.com/FiloSottile/mkcert)
+else
 	mkcert -install
-	# MacOS does not support --parents for the mkdir command
-	mkdir -p $(CERTS_DIR)
 	mkcert -cert-file $(CERTS_DIR)/cert.crt -key-file $(CERTS_DIR)/cert.key localhost
+endif
 .PHONY: certs
+
+${CERTS_DIR}:
+	$(MAKE) certs
+
+${NODE_MODULES_DIR}:
+	mkdir -p ${NODE_MODULES_DIR}
+
+${MANIFEST_FILE}:
+	echo '{}' > $(MANIFEST_FILE)
