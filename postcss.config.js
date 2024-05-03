00d40366be3d8bfc50129ccac58d6f54a6f4e566
SpykeRel04D <roger.vidal.lloveras@gmail.com>
2024-05-03 16:48:08 +0200

    Remove tailwind from archetype

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,6 @@
-const path = require('path');
 const postcssConfig = require('@runroom/npm-scripts').postcssConfig;
 
-module.exports = ({ file }) => {
-  const filename = file.split('/').pop().replace('.css', '');
-  const filepath = filename.indexOf('crp.') >= 0 ? 'crp' : 'base';
-  const configFilename = filename.indexOf('crp.') >= 0 ? filename : 'tailwind';
-
-  return {
-    plugins: {
-      ...postcssConfig.plugins,
-      'tailwindcss/nesting': {},
-      'tailwindcss': {
-        config: path.resolve(__dirname, 'etc', 'tailwind', filepath, `${configFilename}.config.js`)
-      }
-    }
-  };
+module.exports = {
+  ...postcssConfig,
+  plugins: [...postcssConfig.plugins, 'postcss-nesting']
 };
