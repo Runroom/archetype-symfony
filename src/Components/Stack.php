c7f5511865d00105f9b4857e62239f121e29be41
SpykeRel04D <roger.vidal.lloveras@gmail.com>
2024-05-07 16:31:26 +0200

    Fix phpstan

--- src/Components/Stack.php
+++ src/Components/Stack.php
@@ -12,6 +12,16 @@ class Stack
     private const DIRECTION_COLUMN = 'column';
     private const DIRECTION_ROW = 'row';
 
+    public const STACK_DIRECTIONS = [
+        self::DIRECTION_COLUMN,
+        self::DIRECTION_ROW,
+    ];
+
+    public function getDirections(): array
+    {
+        return self::STACK_DIRECTIONS;
+    }
+
     private const SPACING_DENSE = 'dense';
     private const SPACING_STANDARD = 'standard';
     private const SPACING_AIRY = 'airy';
@@ -19,9 +29,34 @@ class Stack
     private const SPACING_3X_STANDARD = '3x-standard';
     private const SPACING_4X_STANDARD = '4x-standard';
 
+    public const STACK_SPACINGS = [
+        self::SPACING_DENSE,
+        self::SPACING_STANDARD,
+        self::SPACING_AIRY,
+        self::SPACING_2X_STANDARD,
+        self::SPACING_3X_STANDARD,
+        self::SPACING_4X_STANDARD,
+    ];
+
+    public function getSpacings(): array
+    {
+        return self::STACK_SPACINGS;
+    }
+
     private const STACK_COLUMNS_AT_NEVER = 'never';
     private const STACK_COLUMNS_AT_MOBILE = 'mobile';
     private const STACK_COLUMNS_AT_TABLET = 'tablet';
+
+    public const STACK_COLUMNS_AT_BREAKPOINTS = [
+        self::STACK_COLUMNS_AT_NEVER,
+        self::STACK_COLUMNS_AT_MOBILE,
+        self::STACK_COLUMNS_AT_TABLET,
+    ];
+
+    public function getStackColumnsAtBreakpoints(): array
+    {
+        return self::STACK_COLUMNS_AT_BREAKPOINTS;
+    }
 
     public string $direction = self::DIRECTION_COLUMN;
     public string $space = self::SPACING_STANDARD;
