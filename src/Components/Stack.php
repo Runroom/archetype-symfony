a1226b32039b81e202b5deb191827866ae0cc50b
SpykeRel04D <roger.vidal.lloveras@gmail.com>
2024-05-07 16:33:02 +0200

    php-cs-fixer

--- src/Components/Stack.php
+++ src/Components/Stack.php
@@ -9,25 +9,10 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 #[AsTwigComponent]
 class Stack
 {
-    private const DIRECTION_COLUMN = 'column';
-    private const DIRECTION_ROW = 'row';
-
     public const STACK_DIRECTIONS = [
         self::DIRECTION_COLUMN,
         self::DIRECTION_ROW,
     ];
-
-    public function getDirections(): array
-    {
-        return self::STACK_DIRECTIONS;
-    }
-
-    private const SPACING_DENSE = 'dense';
-    private const SPACING_STANDARD = 'standard';
-    private const SPACING_AIRY = 'airy';
-    private const SPACING_2X_STANDARD = '2x-standard';
-    private const SPACING_3X_STANDARD = '3x-standard';
-    private const SPACING_4X_STANDARD = '4x-standard';
 
     public const STACK_SPACINGS = [
         self::SPACING_DENSE,
@@ -38,28 +23,42 @@ class Stack
         self::SPACING_4X_STANDARD,
     ];
 
-    public function getSpacings(): array
-    {
-        return self::STACK_SPACINGS;
-    }
-
-    private const STACK_COLUMNS_AT_NEVER = 'never';
-    private const STACK_COLUMNS_AT_MOBILE = 'mobile';
-    private const STACK_COLUMNS_AT_TABLET = 'tablet';
-
     public const STACK_COLUMNS_AT_BREAKPOINTS = [
         self::STACK_COLUMNS_AT_NEVER,
         self::STACK_COLUMNS_AT_MOBILE,
         self::STACK_COLUMNS_AT_TABLET,
     ];
+    private const DIRECTION_COLUMN = 'column';
+    private const DIRECTION_ROW = 'row';
 
-    public function getStackColumnsAtBreakpoints(): array
-    {
-        return self::STACK_COLUMNS_AT_BREAKPOINTS;
-    }
+    private const SPACING_DENSE = 'dense';
+    private const SPACING_STANDARD = 'standard';
+    private const SPACING_AIRY = 'airy';
+    private const SPACING_2X_STANDARD = '2x-standard';
+    private const SPACING_3X_STANDARD = '3x-standard';
+    private const SPACING_4X_STANDARD = '4x-standard';
+
+    private const STACK_COLUMNS_AT_NEVER = 'never';
+    private const STACK_COLUMNS_AT_MOBILE = 'mobile';
+    private const STACK_COLUMNS_AT_TABLET = 'tablet';
 
     public string $direction = self::DIRECTION_COLUMN;
     public string $space = self::SPACING_STANDARD;
     public string $stackColumnsAt = self::STACK_COLUMNS_AT_NEVER;
     public bool $reverseColumnsWhenStacked = false;
+
+    public function getDirections(): array
+    {
+        return self::STACK_DIRECTIONS;
+    }
+
+    public function getSpacings(): array
+    {
+        return self::STACK_SPACINGS;
+    }
+
+    public function getStackColumnsAtBreakpoints(): array
+    {
+        return self::STACK_COLUMNS_AT_BREAKPOINTS;
+    }
 }
