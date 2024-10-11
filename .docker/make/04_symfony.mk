618dcae2c5e611a7c74ac18a2d632a824606042b
Jordi <jordism91@gmail.com>
2024-10-11 16:36:24 +0200

    Try to check database on CI

--- .docker/make/04_symfony.mk
+++ .docker/make/04_symfony.mk
@@ -19,3 +19,8 @@ database: ## Provision the database.
 	$(DOCKER_EXEC) console doctrine:migrations:migrate --no-interaction
 	$(DOCKER_EXEC) console doctrine:fixtures:load --no-interaction
 .PHONY: database
+
+check-database: ## Check the database.
+	$(DOCKER_EXEC) console doctrine:schema:validate
+	$(DOCKER_EXEC) console doctrine:migrations:status
+.PHONY: check
